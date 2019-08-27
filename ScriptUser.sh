1db2cea8042ea4675f145f28c3ed970cc70acf35
root <ygonzalezo@vectoritcgroup.com>
2019-08-27 12:14:36 -0500

    consulta usuarios

--- ScriptUser.sh
+++ ScriptUser.sh
@@ -2,10 +2,7 @@ ORACLE_SID=cdb1; export ORACLE_SID
 env
 sqlplus / as sysdba <<EOF
 set heading off feedback off verify off
-ALTER SESSION SET "_ORACLE_SCRIPT"=true;
-CREATE USER usuario IDENTIFIED BY admin;
-GRANT CONNECT TO usuario;
-GRANT CONNECT, RESOURCE, DBA TO usuario;
+Select * from all_users;
 exit
 EOF
 
