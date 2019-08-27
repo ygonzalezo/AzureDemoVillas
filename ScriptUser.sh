6c0faa64e12f5202b9e4afb5146e3cb28b2f428c
root <ygonzalezo@vectoritcgroup.com>
2019-08-27 12:37:20 -0500

    Commit_creación_usuario

--- ScriptUser.sh
+++ ScriptUser.sh
@@ -2,7 +2,10 @@ ORACLE_SID=cdb1; export ORACLE_SID
 env
 sqlplus / as sysdba <<EOF
 set heading off feedback off verify off
-Select * from all_users;
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+CREATE USER agente IDENTIFIED BY admin;
+GRANT CONNECT TO agente;
+GRANT CONNECT, RESOURCE, DBA TO agente;
 exit
 EOF
 
