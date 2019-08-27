b108de7f3fc3962ae0014d1e74d9d41912265ca3
Milena Gonzalez <ymgonzalez9@outlook.es>
2019-08-27 14:28:21 -0500

    Script_actualizado

--- ScriptUser.sh
+++ ScriptUser.sh
@@ -6,6 +6,7 @@ ALTER SESSION SET "_ORACLE_SCRIPT"=true;
 CREATE USER agente3 IDENTIFIED BY admin;
 GRANT CONNECT TO agente3;
 GRANT CONNECT, RESOURCE, DBA TO agente3;
+Select * from all_users;
 exit
 EOF
 
