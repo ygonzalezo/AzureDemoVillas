3ec31dcf82f52e7bc30997670d8952ad1d9135e6
Milena Gonzalez <ymgonzalez9@outlook.es>
2019-08-27 12:04:41 -0500

    Commitfinal
    Creación de usuario

--- ScriptUser.sh
+++ ScriptUser.sh
@@ -2,7 +2,10 @@ ORACLE_SID=cdb1; export ORACLE_SID
 env
 sqlplus / as sysdba <<EOF
 set heading off feedback off verify off
-SELECT * FROM ALL_USERS;
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+CREATE USER usuario IDENTIFIED BY admin;
+GRANT CONNECT TO milena;
+GRANT CONNECT, RESOURCE, DBA TO milena;
 exit
 EOF
 
