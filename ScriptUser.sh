02217b2f9b19995deea4a687d058bb908043ae34
root <ygonzalezo@vectoritcgroup.com>
2019-08-27 12:07:47 -0500

    Commit nuevo usuario

--- ScriptUser.sh
+++ ScriptUser.sh
@@ -4,8 +4,8 @@ sqlplus / as sysdba <<EOF
 set heading off feedback off verify off
 ALTER SESSION SET "_ORACLE_SCRIPT"=true;
 CREATE USER usuario IDENTIFIED BY admin;
-GRANT CONNECT TO milena;
-GRANT CONNECT, RESOURCE, DBA TO milena;
+GRANT CONNECT TO usuario;
+GRANT CONNECT, RESOURCE, DBA TO usuario;
 exit
 EOF
 
